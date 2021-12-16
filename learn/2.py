0b0cb266d560abdb4ee3017d276602a6a62c91a2
Vestimy <vestimyandrey@gmail.com>
2021-12-16 16:19:48 +0300

    update catalog

--- learn/2.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Введите число: "))
-
-if number%2 > 0:
-    print(f'Число {number} нечетное')
-else:
-    print(f'Число {number} четное')
--- learn/4.py
+++
@@ -1,9 +0,0 @@
-numbers = 36
-new = []
-number = 0
-while numbers != number:
-    number = number + 1
-    if numbers % number == 0:
-        new.append(number)
-
-print(new)
